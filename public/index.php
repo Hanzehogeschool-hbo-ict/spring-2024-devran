639228bbc6269edbf805e154f947e5b5b29dd192
devranaydinoglu <d.s.aydinoglu@gmail.com>
2024-06-26 18:19:59 +0200

    Imported dotenv

--- public/index.php
+++ public/index.php
@@ -3,12 +3,13 @@
 use Hive\App;
 use Hive\Database;
 use Hive\Session;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once('../vendor/autoload.php');
 
 const TEMPLATE_DIR = __DIR__ . '/../templates';
 
-$dotenv = new Symfony\Component\Dotenv\Dotenv();
+$dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/../.env');
 
 $app = App::getInstance();
