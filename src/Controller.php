ec3ddee1f3d4833b259df5b27566fda14be01704
devranaydinoglu <d.s.aydinoglu@gmail.com>
2024-07-01 12:22:04 +0200

    Removed unused member variable

--- src/Controller.php
+++ src/Controller.php
@@ -4,19 +4,15 @@ namespace Hive;
 
 class Controller
 {
-    protected Game $game;
 
     public function __construct(protected ?Database $db, protected ?Session $session)
     {
-        $this->game = $this->session->get('game');
     }
 
     protected function saveToDatabase(string $type, string $piece, string $to): void
     {
-        if (!isset($this->game))
-            $this->game = $this->session->get('game');
-
-        $state = $this->db->escape($this->game);
+        $game = $this->session->get('game');
+        $state = $this->db->escape($game);
         $last = $this->session->get('last_move') ?? 'null';
         $this->db->execute("
                 insert into moves (game_id, type, move_from, move_to, previous_id, state)
